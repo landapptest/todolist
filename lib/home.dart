e5befa72b05e4e12d1a0b5ffb982ae6a66597a22
[APP1] <andyleeqwerty856@gmail.com>
2024-12-04 16:29:31 +0900

    home최종

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'SujPopup.dart';
-import 'todoPopup.dart';
 import 'footer.dart';
 import 'SubjectTimerProvider.dart';
 
@@ -14,9 +12,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0; // 현재 선택된 탭 인덱스
-  bool _showTodoList = false; // To-Do List 화면 여부
   String _totalElapsedTime = '00:00:00'; // 총 시간
-  List<String> _todoList = ["To-do1", "To-do2", "To-do3"]; // To-Do 리스트
 
   @override
   void initState() {
@@ -70,69 +66,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return '$hoursStr:$minutesStr:$secondsStr';
   }
 
-  // 과목 팝업 띄우기 메서드
-  void _showPopup() {
-    final subjectTimerProvider =
-    Provider.of<SubjectTimerProvider>(context, listen: false);
-    showDialog(
-      context: context,
-      builder: (context) {
-        return PopupScreen(
-          subjects: subjectTimerProvider.subjects,
-          onDeleteSubject: (int index) {
-            setState(() {
-              subjectTimerProvider.resetTimer(
-                  subjectTimerProvider.subjects[index]);
-              subjectTimerProvider.deleteSubject(index);
-            });
-            _updateTotalTime();
-          },
-          onAddSubject: (String newSubject) {
-            setState(() {
-              subjectTimerProvider.addSubject(newSubject);
-            });
-          },
-          onEditSubject: (int index, String newName) {
-            setState(() {
-              subjectTimerProvider.editSubject(index, newName);
-            });
-          },
-        );
-      },
-    );
-  }
-
-  // To-Do 팝업 띄우기 메서드
-  void _showTodoPopup() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return tdPopupScreen(
-          todoList: List.from(_todoList),
-          onAddTodo: (newTodo) {
-            setState(() {
-              _todoList.add(newTodo); // To-Do 추가
-            });
-          },
-          onEditTodo: (index, updatedTodo) {
-            setState(() {
-              if (index >= 0 && index < _todoList.length) {
-                _todoList[index] = updatedTodo; // To-Do 수정
-              }
-            });
-          },
-          onDeleteTodo: (index) {
-            setState(() {
-              if (index >= 0 && index < _todoList.length) {
-                _todoList.removeAt(index); // To-Do 삭제
-              }
-            });
-          },
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final subjectTimerProvider = Provider.of<SubjectTimerProvider>(context);
@@ -142,6 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // 상단 타이머 영역
               Container(
@@ -171,69 +105,97 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 32),
 
               // 진행 상황 그래프
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              const Text(
+                '오늘의 진행상황 그래프',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Stack(
                 children: [
-                  const Text(
-                    '오늘의 진행상황 그래프',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                  Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey[700],
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  Stack(
-                    children: [
-                      Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[700],
+                  Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width *
+                        subjectTimerProvider.progress,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromRGBO(80, 255, 53, 1),
+                    ),
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text(
+                        "${(subjectTimerProvider.progress * 100).toStringAsFixed(0)}%",
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        height: 40,
-                        width: MediaQuery.of(context).size.width *
-                            subjectTimerProvider.progress,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromRGBO(80, 255, 53, 1),
-                        ),
-                        alignment: Alignment.centerRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Text(
-                            "${(subjectTimerProvider.progress * 100).toStringAsFixed(0)}%",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  const Divider(color: Colors.greenAccent, thickness: 1),
                 ],
               ),
+              const Divider(color: Colors.greenAccent, thickness: 1),
 
-              // 과목 리스트 or To-Do List
-              _showTodoList ? _buildTodoList() : _buildSubjectList(),
+              const SizedBox(height: 16),
 
-              // 전환 버튼
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _showTodoList = !_showTodoList;
-                  });
+              // 과목 리스트
+              const Text(
+                '과목 리스트',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 8),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: subjectTimerProvider.subjects.length,
+                itemBuilder: (context, index) {
+                  final subjectName = subjectTimerProvider.subjects[index];
+                  final elapsedTime =
+                      subjectTimerProvider.elapsedTimes[subjectName] ??
+                          "00:00:00";
+
+                  return ListTile(
+                    title: Text(
+                      subjectName,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      "Elapsed: $elapsedTime",
+                      style: const TextStyle(color: Colors.white70),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.play_arrow,
+                              color: Colors.greenAccent),
+                          onPressed: () {
+                            subjectTimerProvider.startTimer(subjectName);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.pause, color: Colors.red),
+                          onPressed: () {
+                            subjectTimerProvider.stopTimer(subjectName);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[800],
-                ),
-                child: Text(
-                  _showTodoList ? "과목 보기" : "To-Do 보기",
-                  style: const TextStyle(color: Colors.white),
-                ),
               ),
             ],
           ),
@@ -247,89 +209,6 @@ class _HomeScreenState extends State<HomeScreen> {
           });
         },
       ),
-    );
-  }
-
-  // 과목 리스트
-  Widget _buildSubjectList() {
-    final subjectTimerProvider =
-    Provider.of<SubjectTimerProvider>(context, listen: true);
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: subjectTimerProvider.subjects.length,
-      itemBuilder: (context, index) {
-        final subjectName = subjectTimerProvider.subjects[index];
-        final elapsedTime =
-            subjectTimerProvider.elapsedTimes[subjectName] ?? "00:00:00";
-
-        return ListTile(
-          title: Text(subjectName, style: const TextStyle(color: Colors.white)),
-          subtitle: Text("Elapsed: $elapsedTime"),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.play_arrow, color: Colors.greenAccent),
-                onPressed: () {
-                  subjectTimerProvider.startTimer(subjectName);
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.pause, color: Colors.red),
-                onPressed: () {
-                  subjectTimerProvider.stopTimer(subjectName);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  // To-Do 리스트
-  Widget _buildTodoList() {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: _todoList.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              _todoList[index] = _todoList[index].contains("(완료)")
-                  ? _todoList[index].replaceAll(" (완료)", "")
-                  : "${_todoList[index]} (완료)";
-            });
-          },
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: _todoList[index].contains("(완료)")
-                  ? Colors.grey[800]
-                  : const Color.fromRGBO(80, 255, 53, 1),
-            ),
-            child: ListTile(
-              leading: Icon(
-                _todoList[index].contains("(완료)")
-                    ? Icons.check_circle
-                    : Icons.circle,
-                color: Colors.white,
-              ),
-              title: Text(
-                _todoList[index],
-                style: TextStyle(
-                  color: _todoList[index].contains("(완료)")
-                      ? Colors.white
-                      : Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
